05159ea0f141185f8d0b3c50749ff7a5f56e9351
GickiAnarchy <mindofmagick85@gmail.com>
2026-03-31 04:38:25 -0500

    Refactor data management and enhance employee/employer functions

--- data/__init__.py
+++ data/__init__.py
@@ -1,11 +1,31 @@
 from .models import Employer, Employee
-from .popups import CreateDialog
+from .sheet_utils import get_employees, get_employee, get_employers, get_employer, manage_employee, manage_employer, check_employee_exists, check_employer_exists, print_lists
 
+
+
+MANAGEMENT_FUNCTIONS = [
+    "sheet_utils.get_employees",
+    "sheet_utils.get_employee",
+    "sheet_utils.get_employers",
+    "sheet_utils.get_employer",
+    "sheet_utils.manage_employee",
+    "sheet_utils.manage_employer",
+    "sheet_utils.check_employee_exists",
+    "sheet_utils.check_employer_exists",
+]
 
 
 
 __all__ = [
-    "Employee",
     "Employer",
-    "CreateDialog",
+    "Employee",
+    "get_employees",
+    "get_employee",
+    "get_employers",
+    "get_employer",
+    "manage_employee",
+    "manage_employer",
+    "check_employee_exists",
+    "check_employer_exists"
+    "print_lists",
     ]
--- data/models.py
+++ data/models.py
@@ -1,32 +1,25 @@
-from datetime import datetime
+
+
 
 class Employee:
-    def __init__(self, id=None, name="", wage=0):
-        self.id = id
+    def __init__(self, name, wage=None, **kwargs):
         self.name = name
         self.wage = wage
+        try:
+            self.pay_advances = kwargs.get("pay_advances", [])
+        except KeyError:
+            self.pay_advances = []
+    
+
+
+
 
 
 class Employer:
-    def __init__(self, id=None, name="", location=""):
-        self.id = id
+    def __init__(self, name, location=None):
         self.name = name
         self.location = location
 
 
-class TimeLog:
-    def __init__(self, id=None, employee_id=None, employer_id=None, 
-                 job_type="General", notes="", start_time=None, end_time=None):
-        self.id = id
-        self.employee_id = employee_id
-        self.employer_id = employer_id
-        self.job_type = job_type
-        self.notes = notes
-        # Sets start_time to 'now' if no time is provided
-        self.start_time = start_time or datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.end_time = end_time
 
 
-class MMData:
-    def __init__(self):
-        pass
